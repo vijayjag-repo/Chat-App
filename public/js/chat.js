2eeac6aa5f2b3db2b0146aba2ac771ad7bcaa472
Vijay Jagannathan <vijayjag@Vijays-MacBook-Pro.local>
2019-07-25 00:29:59 -0500

    wait functionality msg

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,30 +2,49 @@
 // ans is the return value from the server or socket.
 const socket = io();
 
-
+//elements
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = document.querySelector('input');
+const $messageFormButton = document.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
 socket.on('message',function(msg){
   console.log(msg);
 });
 
-document.querySelector('#message-form').addEventListener('submit',function(e){
+$messageForm.addEventListener('submit',function(e){
   e.preventDefault();
-
   //e.target gets you to the form and elements gets you to the elements.
+
+  //disable form
+  $messageFormButton.setAttribute('disabled','disabled');
   const message = e.target.elements.tell.value;
-  socket.emit('sendmessage',message);
+  //callback function to know whether the message was delivered.
+  //whoever is emitting an event sets up a callback function.
+  socket.emit('sendmessage',message,function(error){
+    //enable form
+    $messageFormButton.removeAttribute('disabled');
+    $messageFormInput.value = '';
+    $messageFormInput.focus();
+    if(error){
+      return(console.log(error));
+    }
+    console.log("The message was delivered");
+  });
 });
 
 //using geolocation api using browser
-document.querySelector('#send-location').addEventListener('click',function(){
+$sendLocationButton.addEventListener('click',function(){
   //position object contains the location that we want to share.
 
-  // function success(pos){
-  //
-  // }
+  //disable send location button until you receive acknowlegdement
+  $sendLocationButton.setAttribute('disabled','disabled');
   navigator.geolocation.getCurrentPosition(function success(pos){
     socket.emit('sendlocation',{
       lat: pos.coords.latitude,
       lng: pos.coords.longitude
+    },function(ack){
+      $sendLocationButton.removeAttribute('disabled');
+      console.log("Location shared");
     });
   });
 
